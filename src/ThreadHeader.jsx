e17da8d98328147daeaa627a33d881bc10ac1ccf
yuikanaz <osaka.hakuba.18@gmail.com>
2025-01-02 01:56:16 +0900

    station-2 show thread

---
+++ src/ThreadHeader.jsx
@@ -0,0 +1,12 @@
+import "./assets/css/ThreadHeader.css";
+
+function ThreadHeader() {
+  return (
+    <div className="threadHeader">
+      <p>掲示板</p>
+      <a>スレッドを立てる</a>
+    </div>
+  );
+}
+
+export default ThreadHeader;
