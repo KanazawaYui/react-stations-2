ba91f9a93e6e46a2f6f62ddfd724dde461eef0bf
yuikanaz <osaka.hakuba.18@gmail.com>
2025-01-09 22:45:35 +0900

    post

--- src/component/CreateThread.jsx
+++ src/component/CreateThread.jsx
@@ -1,16 +1,51 @@
+import { useState } from "react";
+
 import "../assets/css/CreateThread.css";
 
 const CreateThread = () => {
+  const [newThread, setNewThread] = useState("");
+
+  const API_URL = "https://railway.bulletinboard.techtrain.dev/threads";
+
+  const createThread = async (e) => {
+    e.preventDefault(); // ページリロードを防ぐ
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: newThread }),
+    });
+
+    if (response.ok) {
+      console.log("ok");
+      setNewThread(""); // フォームをリセット
+    } else {
+      console.error("Failed to post comment");
+    }
+  };
+
   return (
     <div className="create">
       <p className="subTitle">スレッド新規作成</p>
-      <input className="createTitle" placeholder="スレッドタイトル"></input>
-      <div className="createButtonSet">
-        <a className="toTop" href="/">
-          Topに戻る
-        </a>
-        <button className="createButton">作成</button>
-      </div>
+      <form onSubmit={createThread}>
+        <input
+          value={newThread}
+          className="createTitle"
+          placeholder="スレッドタイトル"
+          onChange={(e) => {
+            setNewThread(e.target.value);
+          }}
+        />
+        <div className="createButtonSet">
+          <a className="toTop" href="/">
+            Topに戻る
+          </a>
+          <button type="submit" className="createButton">
+            作成
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
