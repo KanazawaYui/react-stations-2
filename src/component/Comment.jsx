6a4c09a2e442119570d049cb25cb8e78d624941d
yuikanaz <osaka.hakuba.18@gmail.com>
2025-01-13 10:05:21 +0900

    add comment

--- src/component/Comment.jsx
+++ src/component/Comment.jsx
@@ -16,29 +16,44 @@ const Comment = ({ thread_id }) => {
       .then((res) => res.json())
       .then((result) => {
         setComments(result.posts.slice(0, 5));
+      })
+      .catch((error) => {
+        console.error("Error: " + error);
       });
   };
 
   const postComment = async (e) => {
     e.preventDefault(); // ページリロードを防ぐ
-    console.log("post newComment:" + newComment);
-    const response = await fetch(
-      `https://railway.bulletinboard.techtrain.dev/threads/${thread_id}/posts`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ post: newComment }),
-      }
-    );
 
-    if (response.ok) {
-      console.log("ok");
-      setNewComment(""); // フォームをリセット
-      getComment(); // コメント一覧を再取得
-    } else {
-      console.error("Failed to post comment");
+    // 空欄でpostされることを防ぐ
+    if (newComment.trim() === "") {
+      alert("コメントを入力してください。");
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://railway.bulletinboard.techtrain.dev/threads/${thread_id}/posts`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ post: newComment }),
+        }
+      );
+
+      if (response.ok) {
+        console.log("ok");
+        setNewComment(""); // フォームをリセット
+        getComment(); // コメント一覧を再取得
+      } else {
+        console.error("Failed to post comment");
+        alert("コメント作成に失敗しました。もう一度行なってください。");
+      }
+    } catch {
+      console.error("Error:", error);
+      alert("コメント作成に失敗しました。もう一度行なってください。");
     }
   };
 
